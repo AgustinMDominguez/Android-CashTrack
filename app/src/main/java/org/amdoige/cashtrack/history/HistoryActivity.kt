82093c47a9da2d9e054646b3633031222091cfdc
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2022-01-12 10:25:12 -0300

    feat: Add HistoryViewModel

--- app/src/main/java/org/amdoige/cashtrack/history/HistoryActivity.kt
+++ app/src/main/java/org/amdoige/cashtrack/history/HistoryActivity.kt
@@ -2,15 +2,18 @@ package org.amdoige.cashtrack.history
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import org.amdoige.cashtrack.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHistoryBinding
+    private lateinit var viewModel: HistoryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         binding.balanceAmount.text = "Current Balance is $99.99"
+        viewModel = ViewModelProvider(this)[HistoryViewModel::class.java]
         setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/org/amdoige/cashtrack/history/HistoryViewModel.kt
@@ -0,0 +1,7 @@
+package org.amdoige.cashtrack.history
+
+import androidx.lifecycle.ViewModel
+
+class HistoryViewModel() : ViewModel() {
+//    history
+}

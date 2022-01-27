52d3fe96a9086909c8ecd9f77ba078ed53f7e127
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2022-01-27 17:27:31 -0300

    feat: Partial implementation of main activity
    Features:
    - Bottom Menu Navigation
    - Home Menu and Wallet Menu
    - Main Action Button

---
+++ app/src/main/java/org/amdoige/cashtrack/mainscreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.amdoige.cashtrack.mainscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.amdoige.cashtrack.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

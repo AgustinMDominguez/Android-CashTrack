0445d725daef8de0829032f7b2af78d79f45e1c8
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2022-01-27 11:37:45 -0300

    feat: Add Wallet Model for database

---
+++ app/src/main/java/org/amdoige/cashtrack/core/database/Wallet.kt
@@ -0,0 +1,27 @@
+package org.amdoige.cashtrack.core.database
+
+import android.content.res.Resources
+import androidx.recyclerview.widget.DiffUtil
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.amdoige.cashtrack.R
+
+@Entity(tableName = "wallet_table")
+data class Wallet(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
+    var name: String = "Wallet",
+    var balance: Double = 0.0,
+    var limit: Double = -1.0,
+    var logo: String = Resources.getSystem().getString(R.string.default_wallet_logo),
+    var colorHex: String = "#008128",
+) {
+
+    companion object {
+        object Comparator : DiffUtil.ItemCallback<Wallet>() {
+            override fun areItemsTheSame(p0: Wallet, p1: Wallet): Boolean = p0.id == p1.id
+
+            override fun areContentsTheSame(p0: Wallet, p1: Wallet): Boolean = p0 == p1
+        }
+    }
+}

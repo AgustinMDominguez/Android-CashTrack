10bcf9cdce1313340a8994e94e1180a325c35d12
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2022-01-28 11:06:29 -0300

    feat: Implement swap of fragments with bottom navigation

--- app/src/main/java/org/amdoige/cashtrack/mainscreen/MainActivity.kt
+++ app/src/main/java/org/amdoige/cashtrack/mainscreen/MainActivity.kt
@@ -2,6 +2,9 @@ package org.amdoige.cashtrack.mainscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import org.amdoige.cashtrack.R
 import org.amdoige.cashtrack.databinding.ActivityMainBinding
 
@@ -11,6 +14,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setUpBottomNav()
+    }
+
+    private fun setUpBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+//        val navController = binding.navHostFragment.findNavController()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_home, R.id.navigation_null, R.id.navigation_billfolds)
+        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
+//
+//    private lateinit var binding: ActivityMainBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+//    }
+//}

7d3f3eb5c7ff7c25982b377520ccd434970293cc
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2022-01-27 23:13:32 -0300

    Chore: Add SafeArgs dependencies to gradle and some minor refactors

--- app/src/main/java/org/amdoige/cashtrack/mainscreen/MainActivity.kt
+++ app/src/main/java/org/amdoige/cashtrack/mainscreen/MainActivity.kt
@@ -3,10 +3,14 @@ package org.amdoige.cashtrack.mainscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.amdoige.cashtrack.R
+import org.amdoige.cashtrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }

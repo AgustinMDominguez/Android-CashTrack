f9de5004b7839dd0f46f7fa21a5607a1d6bd64d5
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2022-02-03 11:14:50 -0300

    feat: Migrate HistoryActivity content to HistoryFragment

--- app/src/main/java/org/amdoige/cashtrack/history/ui/HistoryFragment.kt
+++ app/src/main/java/org/amdoige/cashtrack/history/ui/HistoryFragment.kt
@@ -6,12 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.amdoige.cashtrack.core.database.CashTrackDatabase
+import org.amdoige.cashtrack.core.database.Movement
 import org.amdoige.cashtrack.databinding.FragmentHistoryBinding
-import org.amdoige.cashtrack.mainscreen.MainFragments
+import org.amdoige.cashtrack.history.HistoryViewModel
+import org.amdoige.cashtrack.history.data.HistoryRepository
 import org.amdoige.cashtrack.mainscreen.UIStateViewModel
 import timber.log.Timber
 
 class HistoryFragment : Fragment() {
+    private var movementsAdapter = HistoryMovementsAdapter()
+    private lateinit var viewModel: HistoryViewModel
     private lateinit var binding: FragmentHistoryBinding
     private val uiStateViewModel: UIStateViewModel by activityViewModels {
         UIStateViewModel.Companion.Factory()
@@ -27,13 +40,51 @@ class HistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.recyclerMovements.adapter = movementsAdapter
+        val historyRepository = HistoryRepository(CashTrackDatabase.getInstance(requireContext()))
+        viewModel = ViewModelProvider(
+            this,
+            HistoryViewModel.Companion.Factory(historyRepository)
+        )[HistoryViewModel::class.java]
+        setLivedataObservers()
         setListeners()
     }
 
-    private fun setListeners() {
-        binding.toBillfoldsButton.setOnClickListener {
-            Timber.i("TBR: Navigating to Billfolds!")
-            uiStateViewModel.toBillfoldsScreen()
+    private fun setLivedataObservers() {
+        val movementPagerObserver = Observer<PagingData<Movement>> { pagingData ->
+            movementsAdapter.submitData(lifecycle, pagingData)
         }
+        viewModel.movementsPagingData.observe(this, movementPagerObserver)
+
+        val balanceObserver = Observer<String> { binding.balanceAmount.text = it }
+        viewModel.balanceString.observe(this, balanceObserver)
+    }
+
+    private fun setListeners() {
+        binding.addRandomMovementButton.setOnClickListener { addRandomMovement() }
+
+        binding.addOneHundredMovementsButton.setOnClickListener {
+            Timber.i("Adding many movements!")
+            lifecycleScope.launch {
+                for (a in 1..100) {
+                    addRandomMovement(forceAmount = (1000 - a).toDouble())
+                    withContext(Dispatchers.IO) {
+                        delay(10)
+                    }
+                }
+            }
+            Timber.i("Done Adding Movements")
+        }
+        binding.deleteMovementsButton.setOnClickListener { viewModel.deleteAllMovements() }
+    }
+
+    private fun addRandomMovement(forceAmount: Double? = null) {
+        val amount = forceAmount ?: run {
+            val sign = if ((0..1).random() == 0) 1.0 else -1.0
+            val units = (10..1000).random().toDouble()
+            val cents = (0..99).random().toDouble() / 100.0
+            sign * units + cents
+        }
+        viewModel.newImmediateMovement(amount, "random Movement", "some description")
     }
 }
